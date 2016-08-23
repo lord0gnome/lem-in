6039543117260683ee89aa47c51308eb070fb990
Guillaume RICHARD <guiricha@e1r3p5.42.fr>
2016-08-23 17:36:47 +0200

    scripted as well, for lem-in project

---
+++ gohome.sh
@@ -0,0 +1,10 @@
+cd ~/projects/current_projects/lem-in_github/libft
+git add *
+git commit -m "scripted push message, no really though this is a script"
+git push
+cd ../
+git add sources includes test libft
+git commit -m "scripted as well, for lem-in project"
+git push
+git status
+echo "success???"
